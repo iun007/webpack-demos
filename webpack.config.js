b41041d0b4b72ec1aac9ed149e33ed6159d68470
iun007 <654471467@qq.com>
2017-04-25 22:45:22 +0800

    webpack-demo start

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+var path = require('path');
+module.exports = {
+    //打包的入口文件  String|Object
+    entry: path.resolve(__dirname, 'src/index.js'),
+    output: { //配置打包结果     Object
+        //定义输出文件路径
+        path: path.resolve(__dirname, 'build'),
+        //指定打包文件名称
+        filename: 'bundle.js'
+    },
+};
